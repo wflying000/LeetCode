cfdb43b3c28107927c6008188003edb13ef2a9b2
wflying000 <wf123580@163.com>
2022-10-25 21:02:32 +0800

    添加208题，实现Trie（前缀树）

---
+++ src/Trie.java
@@ -0,0 +1,69 @@
+public class Trie {
+    private class Node {
+        char val;
+        Node[] child;
+        boolean end; //标志当前节点是否为一个单词的结尾
+
+        public Node(char ch) {
+            val = ch;
+            child = new Node[26];
+            end = false;
+        }
+
+        public Node() {
+            child = new Node[26];
+            end = false;
+        }
+    }
+
+    Node root;
+
+    public Trie() {
+        root = new Node();
+    }
+    
+
+    public void insert(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            if (cur.child[ch - 'a'] == null) {
+                cur.child[ch - 'a'] = new Node(ch);
+            }
+            cur = cur.child[ch - 'a'];
+        }
+        cur.end = true;
+    }
+    
+    public boolean search(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            if (cur.child[ch - 'a'] == null) {
+                return false;
+            }
+            cur = cur.child[ch - 'a'];
+        }
+        return cur.end;
+    }
+    
+    public boolean startsWith(String prefix) {
+        Node cur = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char ch = prefix.charAt(i);
+            if (cur.child[ch - 'a'] == null) {
+                return false;
+            }
+            cur = cur.child[ch - 'a'];
+        }
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Trie tree = new Trie();
+        String word = "apple";
+        tree.insert(word);
+        System.out.println(tree.search(word));
+        System.out.println(tree.startsWith("app"));
+    }
+}
