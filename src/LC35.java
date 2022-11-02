e5f96db26075d76c88cdc542bbc3b51b747c7a5e
wflying000 <wf123580@163.com>
2022-11-02 20:58:26 +0800

    添加35题

---
+++ src/LC35.java
@@ -0,0 +1,17 @@
+public class LC35 {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length - 1;
+        int res = nums.length;
+        while (left <= right) {
+            int mid = (right - left) / 2 + left;
+            if (nums[mid] >= target) {
+                res = mid;
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return res;
+    }
+}
