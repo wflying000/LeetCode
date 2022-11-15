2cf39b8db91f5616ad5f3cc52dd1843770509d47
wflying000 <wf123580@163.com>
2022-11-15 21:41:06 +0800

    添加1710题

---
+++ src/greedy/LC1710.java
@@ -0,0 +1,19 @@
+public class LC1710 {
+    public int maximumUnits(int[][] boxTypes, int truckSize) {
+        Arrays.sort(boxTypes, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return b[1] - a[1];
+            }
+        });
+        int res = 0;
+        for (int i = 0; i < boxTypes.length; i++) {
+            int k = Math.min(truckSize, boxTypes[i][0]);
+            res += k * boxTypes[i][1];
+            truckSize -= k;
+            if (truckSize == 0) {
+                break;
+            }
+        }
+        return res;
+    }
+}
