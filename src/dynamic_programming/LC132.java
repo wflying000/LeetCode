b5e31502888c2059cc667abc99943ba09a81c151
wflying000 <wf123580@163.com>
2022-12-08 22:09:03 +0800

    添加132题

---
+++ src/dynamic_programming/LC132.java
@@ -0,0 +1,38 @@
+package dynamic_programming;
+import java.util.Arrays;
+
+public class LC132 {
+    public static int minCut(String s) {
+        int n = s.length();
+        boolean[][] dp = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(dp[i], true);
+        }
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = i + 1; j < n; j++) {
+                dp[i][j] = (s.charAt(i) == s.charAt(j)) && dp[i + 1][j - 1];
+            }
+        }
+
+        int[] f = new int[n];
+        Arrays.fill(f, n + 1);
+        for (int i = 0; i < n; i++) {
+            if (dp[0][i]) {
+                f[i] = 0;
+            } else {
+                for (int j = 0; j < i; j++) {
+                    if (dp[j + 1][i]) {
+                        f[i] = Math.min(f[i], f[j] + 1);
+                    }
+                }
+            }
+        }
+        return f[n - 1];
+    }
+
+    public static void main(String[] args) {
+        String s = "aab";
+        int res = minCut(s);
+        System.out.println(res);
+    }
+}
